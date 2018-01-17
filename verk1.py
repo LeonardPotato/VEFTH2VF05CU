c4c729b4eca0f32b5db69070c32aa747ca1ae350
LeonardPotato <eggertaron@gmail.com>
2018-01-17 09:36:42 +0000

    Create verk1.py

---
+++ verk1.py
@@ -0,0 +1,25 @@
+import os
+from bottle import run, route
+
+@route('/')
+def index():
+    return '<a href="/about">About</a> <br>'\
+           '<a href="/biography">Biography</a> <br>'\
+           '<a href="/pictures">Pictures</a>'
+
+@route('/about')
+def about():
+    return 'About'
+
+
+@route('/biography')
+def biography():
+    return 'gjvv'
+
+
+@route('/pictures')
+def pictures():
+    return 'gjvg'
+
+
+run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
